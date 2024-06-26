77a5a02127c10157f261d7dca61fb1e05779225b
Yuwei Liang <z5433878@ad.unsw.edu.au>
2024-06-27 00:34:15 +0800

    Initial Commit

---
+++ navigation practice/navigation_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigation_practiceApp.swift
+//  navigation practice
+//
+//  Created by yuweiL on 27/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct navigation_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
