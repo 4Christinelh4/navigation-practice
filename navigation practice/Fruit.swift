78972c11518621c66b8c93f2800c769c6297f981
Yuwei Liang <z5433878@ad.unsw.edu.au>
2024-06-28 13:38:39 +0800

    Navigation learning

---
+++ navigation practice/Fruit.swift
@@ -0,0 +1,8 @@
+//
+//  Fruit.swift
+//  navigation practice
+//
+//  Created by yuweiL on 27/6/2024.
+//
+
+import Foundation
---
+++ navigation practice/MoreTests.swift
@@ -0,0 +1,18 @@
+//
+//  MoreTests.swift
+//  navigation practice
+//
+//  Created by yuweiL on 27/6/2024.
+//
+
+import SwiftUI
+
+struct MoreTests: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoreTests()
+}
